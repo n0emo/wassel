a55e87cfa05800f318fdece50bfa2d13161be468
Albert Shefner <dev_n0emo@tuta.io>
2025-04-29 00:06:56 +0300

    Rewrite Python plugin example

--- examples/python-plugin/app.py
+++ examples/python-plugin/app.py
@@ -1,33 +1,54 @@
-from typing import override
+import pathlib
+from typing import override, List
 
+import plugin
 from plugin import exports
-from plugin.exports import http_plugin
-from plugin.imports import types
+from plugin.exports.http_plugin import Endpoint
 from plugin.types import Ok
+from plugin.imports.wasi_http_types import (
+    IncomingRequest,
+    ResponseOutparam,
+    OutgoingResponse,
+    Fields,
+    OutgoingBody,
+)
 
-class PythonHandler(http_plugin.Handler):
-    @override
-    def handle(self, request: types.IncomingRequest, out: types.ResponseOutparam) -> None:
-        res = types.OutgoingResponse(types.Fields())
-        res.set_status_code(200)
-        body = res.body()
-        types.ResponseOutparam.set(out, Ok(res))
-        with body.write() as stream:
-            stream.write(b"Hello from python")
-        types.OutgoingBody.finish(body, None)
+from jinja2 import Environment, DictLoader, select_autoescape
+
+def read_entire_file(path: str) -> str:
+    final_path = pathlib.Path(__file__).parent.resolve().joinpath(path)
+    with open(final_path, 'r') as file:
+        return file.read()
+
+templates = {
+    "index.html": read_entire_file("templates/index.html")
+}
+
+env = Environment(
+    loader=DictLoader(templates),
+    autoescape=select_autoescape()
+)
 
 class HttpPlugin(exports.HttpPlugin):
     @override
-    def instantiate(self) -> http_plugin.Plugin:
-        return exports.http_plugin.Plugin(
-            name="python-plugin",
-            version="0.0.0",
-            description=None,
-            endpoints=[
-                http_plugin.Endpoint(
-                    path="/python",
-                    handler=PythonHandler(),
-                )
-            ],
-        )
+    def get_endpoints(self) -> List[Endpoint]:
+        return [Endpoint("/", "handle-root")]
 
+
+class Plugin(plugin.Plugin):
+    @override
+    def handle_root(
+        self,
+        request: IncomingRequest,
+        response_out: ResponseOutparam
+    ) -> None:
+        template = env.get_template("index.html");
+        html = template.render().encode('utf-8')
+
+        res = OutgoingResponse(Fields())
+        res.set_status_code(200)
+        body = res.body()
+        ResponseOutparam.set(response_out, Ok(res))
+        with body.write() as stream:
+            stream.write(html)
+        OutgoingBody.finish(body, None)
